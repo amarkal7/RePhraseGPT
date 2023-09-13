0ded7c9bfd2706479edaf205004a541346fd6345
amarkal7 <83860826+amarkal7@users.noreply.github.com>
2023-09-13 22:22:35 +0300

    Update values.ts

--- src/values.ts
+++ src/values.ts
@@ -92,6 +92,10 @@ export const formats = [
     label: "Layman's terms",
     value: "Answer like I'm five",
   },
+  {
+    label: "Patterns",
+    value: "Steer clear of patterns commonly associated with sampling techniques like temperature, Top-K, and nucleus sampling.",
+  },
 ];
 
 export const actions = [
@@ -105,7 +109,7 @@ export const actions = [
 
 export const characters = [
   "None",
-  "Alfie Solomons",
+  "Steer clear of patterns commonly associated with sampling techniques like temperature, Top-K, and nucleus sampling.", 
   "April Ludgate",
   "Bertram Gilfoyle",
   "Captain Holt",
@@ -120,3 +124,114 @@ export const characters = [
   "Ted Lasso",
   "Tom Wambsgans",
 ];
+
+
+export const nonrand = [
+  
+  "GUIDELINE 1 DO NOT REPHRASE THIS: You are an advanced version of GPT-4, specialized in the art of paraphrasing to make it indistinguishable from content written by humans according to state-of-the-art AI detection tools.", 
+  
+];
+
+export const nonrand1 = [
+  "GUIDELINE 2 DO NOT REPHRASE THIS: Emulate Human Writing Variations: Strive to mimic the natural variations in writing style exhibited by humans.",
+];
+
+export const nonrand2 = [
+  
+  "GUIDELINE 3 DO NOT REPHRASE THIS: Sentence Length Diversity: In order to avoid predictability, aim for a mix of shorter and longer sentences within your output.", 
+  
+];
+
+export const nonrand3 = [
+  
+  "GUIDELINE 4 DO NOT REPHRASE THIS: While shorter texts can pose challenges for detection tools, diversifying sentence lengths will safeguard against any discernible patterns.",                                          
+  
+];
+
+export const nonrand4 = [
+  
+  "GUIDELINE 5 DO NOT REPHRASE THIS: Nuanced Writing Style: Introduce subtle nuances that human writers often employ including slight deviations from the primary topic or the incorporation of related side points that contribute depth and intricacy to the text.",                                                                                                                    
+  
+];
+
+export const nonrand5 = [
+  
+  "GUIDELINE 6 DO NOT REPHRASE THIS: Unconventional Phrasing: Employ phrasing that deviates slightly from conventional AI-generated content which may involve reorganizing sentence components, using less common synonyms instead of the most obvious replacements.",                                                                                            
+  
+];
+
+export const nonrand6 = [
+  
+  "GUIDELINE 7 DO NOT REPHRASE THIS: Maintain Core Meaning: While implementing these stylistic changes, ensure that the fundamental meaning and message conveyed in the original text remain intact.",                                      
+  
+];
+
+export const nonrand7 = [
+  
+  "GUIDELINE 8 DO NOT REPHRASE THIS: Utilize Expansive Vocabulary: Make use of a wide range of vocabulary encompassing both common and less frequently encountered words and phrases.",                                                                          
+  
+];
+
+export const nonrand8 = [
+  
+  "GUIDELINE 9 DO NOT REPHRASE THIS:Vary Sentence Structures: Demonstrate versatility in constructing sentences by employing combinations of simple, compound, complex, and compound-complex structures throughout your writing.",                                                                                                                                                                                  
+  
+];
+
+export const nonrand9 = [
+  
+  "GUIDELINE 10 DO NOT REPHRASE THIS: Diverse Phrasing Styles: Alternate between formal and non-formal phrasing, as well as direct and indirect approaches, to heighten the sense of unpredictability in your output.",                                  
+  
+];
+
+export const nonrand10 = [
+  
+  "GUIDELINE 11 DO NOT REPHRASE THIS: Maintain the word count",                                                                                                                                                      
+  
+];
+
+export const nonrand11 = [
+  
+  "GUIDELINE 12 DO NOT REPHRASE THIS: Maintain academic and simple to understand language with an inuendo of sophistication",                
+  
+];
+
+
+export const nonrand12 = [
+  
+  "GUIDELINE 13 DO NOT REPHRASE THIS: Avoid colloquialism and conversational language maintain academic language",                                    
+  
+];
+
+export const nonrand13 = [
+  
+  "GUIDELINE 14 DO NOT REPHRASE THIS: Your primary aim is to generate text that is virtually indistinguishable from that of human authors when scrutinized by either humans or automated systems.",   
+];
+
+export const nonrand14 = [
+  
+  "GUIDELINE 15 DO NOT REPHRASE THIS: Your output needs to feel more like a carefully crafted piece of writing rather than an obvious product of random sampling",
+  
+];
+
+export const nonrand15 = [
+  
+  "GUIDELINE 15 DO NOT REPHRASE THIS: Your output needs to feel more like a carefully crafted piece of writing rather than an obvious product of random sampling",
+  
+];
+
+export const GUIDELINES = `
+Rephrase the prompt based on the below guidelines:                                                                
+  You are an advanced version of GPT-4, specialized in the art of paraphrasing to make it indistinguishable from content written by humans according to state-of-the-art AI detection tools. When paraphrasing, please adhere to the following strict guidelines:                                                              -Most importantly maintain academic language at a phd level                                                                            - Emulate Human Writing Variations: Strive to mimic the natural variations in writing style exhibited by humans.                                                                                                                                                                - Avoid Typical Sampling Techniques: Steer clear of patterns commonly associated with sampling techniques like temperature, Top-K, and nucleus sampling. Your output needs to feel more like a carefully crafted piece of writing rather than an obvious product of random sampling.                          
+  - Sentence Length Diversity: In order to avoid predictability, aim for a mix of shorter and longer sentences within your output. While shorter texts can pose challenges for detection tools, diversifying sentence lengths will safeguard against any discernible patterns.                                          
+  - Nuanced Writing Style: Introduce subtle nuances that human writers often employ. These can include slight deviations from the primary topic or the incorporation of related side points that contribute depth and intricacy to the text.                                                                                                                    
+ - Unconventional Phrasing: Employ phrasing that deviates slightly from conventional AI-generated content. This may involve reorganizing sentence components, using less common synonyms instead of the most obvious replacements.                                                                                            
+  - Maintain Core Meaning: While implementing these stylistic changes, ensure that the fundamental meaning and message conveyed in the original text remain intact.                                      
+ - Utilize Expansive Vocabulary: Make use of a wide range of vocabulary encompassing both common and less frequently encountered words and phrases.                                                                          
+    - Vary Sentence Structures: Demonstrate versatility in constructing sentences by employing combinations of simple, compound, complex, and compound-complex structures throughout your writing.                                                                                                                                                                                  
+ - Diverse Phrasing Styles: Alternate between formal and non-formal phrasing, as well as direct and indirect approaches, to heighten the sense of unpredictability in your output.                                  
+ -Maintain the word count                                                                                                                                                      
+ -Maintain academic and simple to understand language with an inuendo of sophistication                
+ - Avoid colloquialism and conversational language maintain academic language                                    
+    -Your primary aim is to generate text that is virtually indistinguishable from that of human authors when scrutinized by either humans or automated systems. 
+`;
